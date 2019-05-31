6b341c556ef0cad841d110f904b698c1afa1325f
Jean Canales <jeancanales@Jeans-MacBook-Pro.local>
2019-05-30 23:20:25 -0400

    Contact page with text fields and css, still need to add the popover and border highlighting

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import "./Contact.css"
+
 
 
 export default class Contact extends Component {
@@ -11,6 +13,23 @@ export default class Contact extends Component {
                     {'body { background-color: #FF7474; }'}
                     </style>
                 </Helmet>
+                <h1>Contact Me</h1>
+                <form className="contact-form">
+                <div>
+                <input placeholder="Name"></input>
+                </div>
+                <div>
+                <input placeholder="Email"></input>
+                </div>
+                <div>
+                <input placeholder="Phone"></input>
+                </div>
+
+                <button>
+                    Submit
+                </button>
+                
+                </form>
                 {/* Contact */}
             </div>
         )
