03096f3adb888f426a0c053c77c6760ea55bb31e
Jean Canales <jeancanales@Jeans-MacBook-Pro.local>
2019-06-03 19:41:12 -0400

    More links added

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './SideDrawer.css';
 
@@ -15,10 +16,10 @@ const sideDrawer = props => {
         <ul>
             <li><a href="/">Jean Canales</a></li>
             <li><a href="/">Home</a></li>
-            <li><a href="/">Contact</a></li>
+            <li><Link to="/Contact">Contact</Link></li>
             <li><a href="/">Resume</a></li>
-            <li><a href="/">LinkedIn</a></li>
-            <li><a href="/">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/jean-canales-6b1441a7/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/Hakuti" target="_blank">Github</a></li>
         </ul>
         {/* <div className="side-drawer__x-button">
             X
