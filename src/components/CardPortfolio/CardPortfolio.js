84d42d144038b1cc28a1c0888c9e11d956782261
Jean Canales <jean@bizzynate.com>
2022-06-15 22:36:21 -0400

    converting code to newer react in order to remove errors.

--- src/components/CardPortfolio/CardPortfolio.js
+++ src/components/CardPortfolio/CardPortfolio.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import './CardPortfolio.css'
-import { Player } from 'video-react';
-import { pause, play } from 'video-react/lib/actions/player';
+import React from "react";
+import "./CardPortfolio.css";
+import { Player } from "video-react";
+import { pause, play } from "video-react/lib/actions/player";
 
- const CardPortfolio = props => {
-    
-
-
-
-    
- return (
-     <div className="card-div-container">
-     <div class="card-img-holder-container">
-     {/* <Player
+const CardPortfolio = (props) => {
+  return (
+    <div className="card-div-container">
+      <div className="card-img-holder-container">
+        {/* <Player
       
       poster="/assets/poster.png"
       src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
     /> */}
-      {/* <video                 
+        {/* <video                 
     src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
     muted={true}
     autoPlay={true}
     
     ></video>  */}
-    <img src={props.image}/>
+        <img src={props.image} />
         {/* <img src="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2x" alt="Img-Box"/> */}
-     </div>
-     <div className="card-portfolio-title">{props.title}</div>
+      </div>
+      <div className="card-portfolio-title">{props.title}</div>
 
-     <div className="line-decorator-div"></div>
-     <div className="card-content-div">
-     <p>{props.description}</p>
-     </div>
+      <div className="line-decorator-div"></div>
+      <div className="card-content-div">
+        <p>{props.description}</p>
+      </div>
 
-     <div className="card-footer-div">
-         <button><a href={props.linkDemo} target="_blank">Demo</a></button>
-         <button><a href={props.linkGit} target="_blank">Github</a></button>
-         
-     </div>
-
-     </div>
- )
-}
+      <div className="card-footer-div">
+        <button>
+          <a href={props.linkDemo} target="_blank" rel="noreferrer">
+            Demo
+          </a>
+        </button>
+        <button>
+          <a href={props.linkGit} target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default CardPortfolio;
